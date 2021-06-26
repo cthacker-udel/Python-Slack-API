f4d92a4dda59a36619300506d521d483e91c1386
cthacker-udel <cthacker@udel.edu>
2021-06-26 13:49:58 -0400

    Added : SlackConversations API : Query Params generation in SlackConversations.py

--- SlackConversations.py
+++ SlackConversations.py
@@ -8,11 +8,27 @@ class SlackConversations(SlackClient):
         self.is_private = None
         self.team_id = None
 
+        self.cursor = None
+        self.inclusive = None
+        self.latest = None
+        self.limit = None
+        self.oldest = None
+
 
     def generate_queries(self):
 
         body = {}
 
+        if self.cursor != None:
+            body['cursor'] = self.cursor
+        if self.inclusive != None:
+            body['inclusive'] = self.inclusive
+        if self.latest != None:
+            body['latest'] = self.latest
+        if self.limit != None:
+            body['limit'] = self.limit
+        if self.oldest != None:
+            body['oldest'] = self.oldest
         if self.channel != None:
             body['channel'] = self.channel
         if self.name != None:
@@ -29,3 +45,8 @@ class SlackConversations(SlackClient):
         self.name = None
         self.is_private = None
         self.team_id = None
+        self.cursor = None
+        self.inclusive = None
+        self.latest = None
+        self.limit = None
+        self.oldest = None
