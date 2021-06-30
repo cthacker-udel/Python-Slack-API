1b6a5365c99990639b1ed9fd4e96a729b036e5c1
cthacker-udel <cthacker@udel.edu>
2021-06-29 22:54:59 -0400

    Added : Files API : Get File Information

--- SlackFiles.py
+++ SlackFiles.py
@@ -6,6 +6,11 @@ class SlackFiles(SlackClient):
         self.file = None
         self.id = None
 
+        self.count = None
+        self.cursor = None
+        self.limit = None
+        self.page = None
+
     def generate_queries(self):
 
         body = {}
@@ -14,9 +19,21 @@ class SlackFiles(SlackClient):
             body['file'] = self.file
         if self.id != None:
             body['id'] = self.id
+        if self.count != None:
+            body['count'] = self.count
+        if self.cursor != None:
+            body['cursor'] = self.cursor
+        if self.limit != None:
+            body['limit'] = self.limit
+        if self.page != None:
+            body['page'] = self.page
         return body
 
     def clear_queries(self):
         self.file = None
         self.id = None
+        self.count = None
+        self.cursor = None
+        self.limit = None
+        self.page = None
 
