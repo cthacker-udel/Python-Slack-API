cdcafb939a0c38b1f17696ee121420dd7f129e2e
cthacker-udel <cthacker@udel.edu>
2021-07-10 11:02:12 -0400

    Added : Users API : delete profile picture

--- SlackRestAPI.py
+++ SlackRestAPI.py
@@ -1936,6 +1936,16 @@ class SlackUsers(SlackClient):
 
         pprint(request)
 
+    def delete_profile_picture(self):
+
+        url = base_url + '/users.deletePhoto'
+
+        body = self.client.SlackUsers.generate_queries()
+
+        request = requests.get(url,auth=HTTPBasicAuth('',self.client.token),params=body)
+
+        pprint(request)
+
 
 
 
