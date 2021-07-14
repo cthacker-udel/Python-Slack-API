5392c55e64c1cbd60e056ee8dc9388a5924fa3ff
cthacker-udel <cthacker@udel.edu>
2021-07-13 22:42:26 -0400

    Added : Groups API : fetch history of messages and event from private channel

--- SlackGroups.py
+++ SlackGroups.py
@@ -9,11 +9,27 @@ class SlackGroups(SlackClient):
         self.team_id = None
         self.validate = None
 
+        self.count = None
+        self.inclusive = None
+        self.latest = None
+        self.oldest = None
+        self.unreads = None
+
 
     def generate_queries(self):
 
         body = {}
 
+        if self.count != None:
+            body['count'] = self.count
+        if self.inclusive != None:
+            body['inclusive'] = self.inclusive
+        if self.latest != None:
+            body['latest'] = self.latest
+        if self.oldest != None:
+            body['oldest'] = self.oldest
+        if self.unreads != None:
+            body['unreads'] = self.unreads
         if self.name != None:
             body['name'] = self.name
         if self.team_id != None:
@@ -25,6 +41,11 @@ class SlackGroups(SlackClient):
         return body
 
     def clear_queries(self):
+        self.count = None
+        self.inclusive = None
+        self.latest = None
+        self.oldest = None
+        self.unreads = None
         self.name = None
         self.team_id = None
         self.validate = None
