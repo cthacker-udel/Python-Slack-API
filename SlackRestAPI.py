c8eb3ef647877661a12d3ed186676cdd12cf84b7
cthacker-udel <cthacker@udel.edu>
2021-07-13 22:43:35 -0400

    Added : Groups API : get private channel info

--- SlackRestAPI.py
+++ SlackRestAPI.py
@@ -1761,6 +1761,16 @@ class SlackGroups(SlackClient):
 
         pprint(request)
 
+    def get_private_channel_info(self):
+
+        url = base_url + '/groups.info'
+
+        body = self.client.SlackGroups.generate_queries()
+
+        request = requests.get(url,auth=HTTPBasicAuth('',self.client.token),params=body)
+
+        pprint(request)
+
 
 class SlackMigration(SlackClient):
 
